8d3264043ac12570bb7bafcc200010d60c82bfa3
ikmsuarez23 <ikmsuarez23@lhusurbil.eus>
2025-12-21 10:27:07 +0100

    commit metodo reiniciar y comprobar ganador

--- Tres_En_Raya/main.py
+++ Tres_En_Raya/main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from Tablero import Tablero
 from Jugador import Jugador
 
@@ -12,6 +13,13 @@ tablero = Tablero()
 
 cuadrado = [None] * 9
 turno = "X"
+partida_terminada = False
+
+Combinaciones_Ganadoras =  [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+]
 
 
 ancho_celda = 800 // 3
@@ -28,6 +36,23 @@ def elegir_cuadrado(pos, cuadrado, turno):
         cuadrado[i] = turno
         return True
     return False
+
+def Reiniciar():
+    global cuadrado, turno,partida_terminada
+    cuadrado = [None] * 9
+    turno = "X"
+    partida_terminada =False
+    
+def Comprobar_Ganador():
+    for a,b,c in Combinaciones_Ganadoras:
+        if cuadrado[a] == cuadrado[b] == cuadrado[c] is not None:
+            return cuadrado[a]
+        
+    if None not in cuadrado:
+        return "EMPATE"
+    return None
+            
+    
 
 while running:
     for event in pygame.event.get():
@@ -57,6 +82,14 @@ while running:
                 jugador = Jugador(pygame.Rect(pos_x - 40, pos_y - 40, 80, 80), "X")
 
             jugador.dibujo_Figuras(ventana)
+            
+    resultado = Comprobar_Ganador()
+    if resultado and not partida_terminada:
+        print("Ganador:", resultado)
+        partida_terminada = True
+        time.sleep(1)
+        Reiniciar()
+        
 
     pygame.display.flip()
 
